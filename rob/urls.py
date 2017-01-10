9f872a1c7594cdc6b0f689ace6400795b1aa19d2
Liam Taylor <liam.taylor888@googlemail.com>
2017-01-10 11:50:57 +0000

    redirects to basic homepage upon login

---
+++ rob/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Additional imports for users:
+from django.conf.urls import include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+]
